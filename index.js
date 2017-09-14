157c38afbb4d1ee4187b4921c9599cc3710e3645
allevo <tomallevi@gmail.com>
2017-09-14 16:59:54 +0200

    Init

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const Ajv = require('ajv')
+
+function loadAndValidateEnvironment (fastify, opts, done) {
+  const ajv = new Ajv({ removeAdditional: true, useDefaults: true, coerceTypes: true })
+  const schema = opts.schema
+  const confKey = opts.confKey || 'config'
+
+  const data = Object.assign({}, opts.data || process.env)
+
+  const valid = ajv.validate(schema, data)
+  if (!valid) {
+    const error = new Error(ajv.errors.map(e => e.message).join('\n'))
+    error.errors = ajv.errors
+    return done(error)
+  }
+
+  fastify.decorate(confKey, data)
+
+  done()
+}
+
+module.exports = fp(loadAndValidateEnvironment)
---
+++ test/fastify-env.test.js
@@ -0,0 +1,147 @@
+'use strict'
+
+const t = require('tap')
+const Fastify = require('fastify')
+const fastifyEnv = require('../index')
+
+function makeTest (t, schema, data, isOk, confExpected, errorMessage) {
+  t.plan(isOk ? 2 : 1)
+
+  const fastify = Fastify()
+  fastify.register(fastifyEnv, {
+    confKey: 'config',
+    schema: schema,
+    data: data
+  }, function (err) {
+    if (isOk) {
+      t.notOk(err)
+      t.strictSame(fastify.config, confExpected)
+      t.end()
+      return
+    }
+
+    t.strictSame(err.message, errorMessage)
+  })
+}
+
+const tests = [
+  {
+    name: 'empty ok',
+    schema: { type: 'object' },
+    data: { },
+    isOk: true,
+    confExpected: {}
+  },
+  {
+    name: 'simple object - ok',
+    schema: {
+      type: 'object',
+      properties: {
+        PORT: {
+          type: 'string'
+        }
+      }
+    },
+    data: {
+      PORT: '44'
+    },
+    isOk: true,
+    confExpected: {
+      PORT: '44'
+    }
+  },
+  {
+    name: 'simple object - ok - coerce value',
+    schema: {
+      type: 'object',
+      properties: {
+        PORT: {
+          type: 'integer'
+        }
+      }
+    },
+    data: {
+      PORT: '44'
+    },
+    isOk: true,
+    confExpected: {
+      PORT: 44
+    }
+  },
+  {
+    name: 'simple object - ok - remove additional properties',
+    schema: {
+      type: 'object',
+      properties: {
+        PORT: {
+          type: 'integer'
+        }
+      },
+      additionalProperties: false
+    },
+    data: {
+      PORT: '44',
+      ANOTHER_PORT: '55'
+    },
+    isOk: true,
+    confExpected: {
+      PORT: 44
+    }
+  },
+  {
+    name: 'simple object - ok - use default',
+    schema: {
+      type: 'object',
+      properties: {
+        PORT: {
+          type: 'integer',
+          default: 5555
+        }
+      }
+    },
+    data: { },
+    isOk: true,
+    confExpected: {
+      PORT: 5555
+    }
+  },
+  {
+    name: 'simple object - ok - required + default',
+    schema: {
+      type: 'object',
+      required: [ 'PORT' ],
+      properties: {
+        PORT: {
+          type: 'integer',
+          default: 6666
+        }
+      }
+    },
+    data: { },
+    isOk: true,
+    confExpected: {
+      PORT: 6666
+    }
+  },
+  {
+    name: 'simple object - KO',
+    schema: {
+      type: 'object',
+      required: [ 'PORT' ],
+      properties: {
+        PORT: {
+          type: 'integer'
+        }
+      }
+    },
+    data: { },
+    isOk: false,
+    errorMessage: 'should have required property \'PORT\''
+  }
+]
+
+tests.forEach(function (testConf) {
+  t.test(testConf.name, t => {
+    makeTest(t, testConf.schema, testConf.data, testConf.isOk, testConf.confExpected, testConf.errorMessage)
+  })
+})
