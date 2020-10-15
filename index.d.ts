4cfac3529a424af9654fe3c48be7f0c3e0f5de1c
Pelle Wessman <pelle@kodfabrik.se>
2020-10-15 18:10:01 +0200

    Fix misspelling of "fastify" in types (#73)

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { EnvSchemaOpt } from "env-schema";
 import { FastifyPlugin } from "fastify";
 
-export type fasitfyEnvOpt = EnvSchemaOpt & { confKey?: string}
+export type fastifyEnvOpt = EnvSchemaOpt & { confKey?: string}
 
-export const fasitfyEnv: FastifyPlugin<fasitfyEnvOpt>;
-export default fasitfyEnv;
+export const fastifyEnv: FastifyPlugin<fastifyEnvOpt>;
+export default fastifyEnv;
